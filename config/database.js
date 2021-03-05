f4d5211e0a0a52f8acc68619cc2aa15d76fbc9ee
MatheusA <matheus.garcia@codeby.com.br>
2021-03-05 15:58:30 -0300

    add

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ const { Client } = require('pg');
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: true
   }
 });
 
