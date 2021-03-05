ceff539ecc8e517acd87ae90ed8f64f1472572d0
MatheusA <matheus.garcia@codeby.com.br>
2021-03-05 13:52:06 -0300

    add

--- server.js
+++ server.js
@@ -20,6 +20,21 @@
 const { Ignitor } = require('@adonisjs/ignitor')
 const https = require('https')
 const pem = require('pem')
+const path = require('path')
+const fs = require('fs')
+
+// Certificate
+const options = {
+  key: fs.readFileSync(path.join(__dirname, './server.key')),
+  cert: fs.readFileSync(path.join(__dirname, './server.crt'))
+}
+
+new Ignitor(require('@adonisjs/fold'))
+  .appRoot(__dirname)
+  .fireHttpServer((handler) => {
+    return https.createServer(options, handler)
+  })
+  .catch(console.error)
 
 pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
   if (error) {
