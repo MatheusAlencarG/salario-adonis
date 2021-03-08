6de52755cf2a8560ec195802721aa61301a2767f
MatheusA <matheus.garcia@codeby.com.br>
2021-03-08 09:40:13 -0300

    add

--- config/database.js
+++ config/database.js
@@ -74,7 +74,7 @@ module.exports = {
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      database: Env.get('DB_DATABASE', 'adonis'),
     }
   },
 
@@ -93,9 +93,10 @@ module.exports = {
     connection: {
       host: Env.get('DB_HOST', 'localhost'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', client),
+      user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      database: Env.get('DB_DATABASE', 'adonis'),
+      sslmode = require
     }
   }
 }
