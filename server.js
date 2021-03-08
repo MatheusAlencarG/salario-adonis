ea116c7bbab7a38e6b7d0a0d2680aa5c9d7ebdbe
MatheusA <matheus.garcia@codeby.com.br>
2021-03-08 09:58:46 -0300

    add

--- server.js
+++ server.js
@@ -17,39 +17,46 @@
 |     Make sure to pass relative path from the project root.
 */
 
+// const { Ignitor } = require('@adonisjs/ignitor')
+// const path = require('path')
+// const https = require('https')
+// const fs = require('fs')
+// const pem = require('pem')
+
+// pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
+//   if (error) {
+//     return console.log(error)
+//   }
+
+//   const options = {
+//     key: keys.serviceKey,
+//     cert: keys.certificate
+//   }
+
+//   new Ignitor(require('@adonisjs/fold'))
+//     .appRoot(__dirname)
+//     .fireHttpServer((handler) => {
+//       return https.createServer(options, handler)
+//     })
+//     .catch(console.error)
+// })
+
+// // Certificate
+// const options = {
+//   key: fs.readFileSync(path.join(__dirname, './server.key')),
+//   cert: fs.readFileSync(path.join(__dirname, './server.crt'))
+// }
+
+// new Ignitor(require('@adonisjs/fold'))
+//   .appRoot(__dirname)
+//   .fireHttpServer((handler) => {
+//     return https.createServer(options, handler)
+//   })
+//   .catch(console.error)
+
 const { Ignitor } = require('@adonisjs/ignitor')
-const path = require('path')
-const https = require('https')
-const fs = require('fs')
-const pem = require('pem')
-
-pem.createCertificate({ days: 1, selfSigned: true }, (error, keys) => {
-  if (error) {
-    return console.log(error)
-  }
-
-  const options = {
-    key: keys.serviceKey,
-    cert: keys.certificate
-  }
-
-  new Ignitor(require('@adonisjs/fold'))
-    .appRoot(__dirname)
-    .fireHttpServer((handler) => {
-      return https.createServer(options, handler)
-    })
-    .catch(console.error)
-})
-
-// Certificate
-const options = {
-  key: fs.readFileSync(path.join(__dirname, './server.key')),
-  cert: fs.readFileSync(path.join(__dirname, './server.crt'))
-}
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  .fireHttpServer((handler) => {
-    return https.createServer(options, handler)
-  })
+  .fireHttpServer()
   .catch(console.error)
